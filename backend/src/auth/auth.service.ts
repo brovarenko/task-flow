2df248a8f245b7d5ad54c107a2156dfcb1fa73b3
vladyslav-brovarenko <brovwg@gmail.com>
2024-02-03 10:55:57 +0200

    add jwt auth

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/auth.dto';
+import { compare } from 'bcrypt';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(dto: LoginDto) {
+    const user = await this.validateUser(dto);
+    const payload = {
+      username: user.email,
+      sub: {
+        name: user.name,
+      },
+    };
+
+    return {
+      user,
+      backendToken: {
+        accessToken: await this.jwtService.signAsync(payload, {
+          expiresIn: '1h',
+          secret: process.env.jwtSecretKey,
+        }),
+      },
+      backendToken: {
+        accessToken: await this.jwtService.signAsync(payload, {
+          expiresIn: '1h',
+          secret: process.env.jwtSecretKey,
+        }),
+      },
+    };
+  }
+
+  async validateUser(dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.username);
+    if (user && (await compare(dto.password, user.password))) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    throw new UnauthorizedException();
+  }
+}
