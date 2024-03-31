db10d107dbd0e97b8f69a3c9bbf32134fe494457
Vlad <brovwg@gmail.com>
2024-03-31 23:39:47 +0200

    add nextauth route

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,46 +1,80 @@
 import { Backend_URL } from '@/lib/constants';
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import { NextAuthOptions } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import NextAuth from 'next-auth/next';
 import CredentialsProvider from 'next-auth/providers/credentials';
+
+async function refreshToken(token: JWT): Promise<JWT> {
+	const res = await fetch(Backend_URL + '/auth/refresh', {
+		method: 'POST',
+		headers: {
+			authorization: `Refresh ${token.backendTokens.refreshToken}`
+		}
+	});
+	console.log('refreshed');
+
+	const response = await res.json();
+
+	return {
+		...token,
+		backendTokens: response
+	};
+}
 
 export const authOptions: NextAuthOptions = {
 	providers: [
 		CredentialsProvider({
-			// The name to display on the sign in form (e.g. 'Sign in with...')
 			name: 'Credentials',
-			// The credentials is used to generate a suitable form on the sign in page.
-			// You can specify whatever fields you are expecting to be submitted.
-			// e.g. domain, username, password, 2FA token, etc.
-			// You can pass any HTML attribute to the <input> tag through the object.
 			credentials: {
-				username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+				username: {
+					label: 'Username',
+					type: 'text',
+					placeholder: 'jsmith'
+				},
 				password: { label: 'Password', type: 'password' }
 			},
 			async authorize(credentials, req) {
 				if (!credentials?.username || !credentials?.password) return null;
 				const { username, password } = credentials;
-				// You need to provide your own logic here that takes the credentials
-				// submitted and returns either a object representing a user or value
-				// that is false/null if the credentials are invalid.
-				// e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-				// You can also use the `req` object to obtain additional parameters
-				// (i.e., the request IP address)
-				const res = await fetch(Backend_URL + 'auth/login', {
+				const res = await fetch(Backend_URL + '/auth/login', {
 					method: 'POST',
-					body: JSON.stringify({ username, password }),
-					headers: { 'Content-Type': 'application/json' }
+					body: JSON.stringify({
+						username,
+						password
+					}),
+					headers: {
+						'Content-Type': 'application/json'
+					}
 				});
-				const user = await res.json();
+				if (res.status == 401) {
+					console.log(res.statusText);
 
-				// If no error and we have user data, return it
-				if (res.ok && user) {
-					return user;
+					return null;
 				}
-				// Return null if user data could not be retrieved
-				return null;
+				const user = await res.json();
+				return user;
 			}
 		})
-	]
+	],
+
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) return { ...token, ...user };
+
+			if (new Date().getTime() < token.backendTokens.expiresIn) return token;
+
+			return await refreshToken(token);
+		},
+
+		async session({ token, session }) {
+			session.user = token.user;
+			session.backendTokens = token.backendTokens;
+
+			return session;
+		}
+	}
 };
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
