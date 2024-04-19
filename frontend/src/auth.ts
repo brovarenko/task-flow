e2f5e07189b7d821f147b87941cb450257b88834
Vlad <brovwg@gmail.com>
2024-04-19 22:21:05 +0200

    fix login btn

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,8 +1,37 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [GitHub],
+	providers: [
+		Credentials({
+			// You can specify which fields should be submitted, by adding keys to the `credentials` object.
+			// e.g. domain, username, password, 2FA token, etc.
+			credentials: {
+				email: {},
+				password: {}
+			},
+			authorize: async credentials => {
+				let user = null;
+
+				// logic to salt and hash password
+				const pwHash = saltAndHashPassword(credentials.password);
+
+				// logic to verify if user exists
+				user = await getUserFromDb(credentials.email, pwHash);
+
+				if (!user) {
+					// No user found, so this is their first attempt to login
+					// meaning this is also the place you could do registration
+					throw new Error('User not found.');
+				}
+
+				// return user object with the their profile data
+				return user;
+			}
+		}),
+		GitHub
+	],
 	callbacks: {
 		jwt({ token, user }) {
 			if (user) {
@@ -19,6 +48,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 			console.log(session);
 			console.log(`session_token:`);
 			console.log(token);
+
 			//console.log(new Date().getTime() - token.?exp);
 
 			return session;
--- frontend/src/components/login-btn/login-btn.tsx
+++ frontend/src/components/login-btn/login-btn.tsx
@@ -3,48 +3,18 @@ import Link from 'next/link';
 import React from 'react';
 
 const LoginButton = () => {
-	// const { data: session } = useSession();
-	// console.log({ session });
-
-	// if (session && session.user)
-	// 	return (
-	// 		<div className='flex gap-4 ml-auto'>
-	// 			<p className='text-sky-600'>{session.user.name}</p>
-	// 			<Link
-	// 				href={'/api/auth/signout'}
-	// 				className='flex gap-4 ml-auto text-red-600'
-	// 			>
-	// 				Sign Out
-	// 			</Link>
-	// 		</div>
-	// 	);
-
-	// return (
-	// 	<div className='flex gap-4 ml-auto items-center'>
-	// 		<Link
-	// 			href={'/api/auth/signin'}
-	// 			className='flex gap-4 ml-auto text-orange-600'
-	// 		>
-	// 			Sign In
-	// 		</Link>
-	// 		<Link
-	// 			href={'/signup'}
-	// 			className='flex gap-4 ml-auto bg-orange-600 text-green-200 p-2 rounded'
-	// 		>
-	// 			Sign Up
-	// 		</Link>
-	// 	</div>
-	// );
 	return (
-		<form
-			className='flex gap-4 ml-auto items-center'
-			action={async () => {
-				'use server';
-				await signIn('github');
-			}}
-		>
-			<button type='submit'>Signin with GitHub</button>
-		</form>
+		<div className='flex gap-4 ml-auto items-center'>
+			<form
+				className='flex gap-4 ml-auto bg-orange-600 text-green-200 p-2 rounded'
+				action={async () => {
+					'use server';
+					await signIn();
+				}}
+			>
+				<button type='submit'>Sign in</button>
+			</form>
+		</div>
 	);
 };
 
